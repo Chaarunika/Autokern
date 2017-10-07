1754da4cc4e989dbe8473d2bbf5f56fff4c0c567
malith <you@example.com>
2017-10-08 00:01:23 +0530

    test

--- Autokern/main.cpp
+++ Autokern/main.cpp
@@ -7,9 +7,35 @@
 //
 
 #include <iostream>
+#include <thread>
+
+void func(std::string msg);
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    std::thread t1(func, "Hello");
+    std::thread t2(func, "Hello");
+    std::thread t3(func, "Hello");
+    std::thread t4(func, "Hello");
+
+    t1.join();
+    t2.join();
+    t3.join()  ;
+    t4.join();
+    
     return 0;
+}
+
+void func(std::string msg){
+    
+    int count=0;
+    std::cout << msg << "\n" ;
+    for(double i=0; i<100000;i++){
+        for(double i=0; i<100000;i++){
+            count++;
+            
+        }
+        
+    }
 }
