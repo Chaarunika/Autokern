21c3187a2f9c182712b5eb23fce346155b5d5b9d
malith <you@example.com>
2017-10-07 19:20:28 +0530

    Initial Commit

---
+++ Autokern/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Autokern
+//
+//  Created by malith on 10/7/17.
+//  Copyright © 2017 NMR. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
