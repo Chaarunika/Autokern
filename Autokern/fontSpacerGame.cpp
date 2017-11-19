3c4be36cb41b574f1323f7dcff778cf60db60ca1
MalithRanathunga <malith009@gmail.com>
2017-11-19 15:17:04 +0530

    genarate function completed

--- Autokern/fontSpacerGame.cpp
+++ Autokern/fontSpacerGame.cpp
@@ -12,12 +12,39 @@
 using namespace cv;
 using namespace std;
 
+const int alphabetSize = 3;  //should be greater than 2. otherwise fails intializeBaseDistanceArr()
+int maxWidthArr[alphabetSize];
+int baseDistanceArr[alphabetSize];
+int kernValueArr[alphabetSize-1]={0};
+
+cv::Mat glyphMatArr[alphabetSize];
+
+
 void printGlyph(cv::Mat glyph){
     
     for (int i =0 ; i<glyph.rows; i++) {
         for (int j = 0; j < glyph.cols; j++)
-            cout << (int)glyph.at<uchar>(i,j) << " ";
+            if((int)glyph.at<uchar>(i,j) ==255)
+                cout << "." << " ";
+            else
+                cout << (int)glyph.at<uchar>(i,j) << " ";
         cout << endl ;
+    }
+}
+
+void printStats(){
+    cout << "\nbaseDistanceArr  maxWidthArr\n";
+    for(int i=0; i<alphabetSize; i++){
+        cout << baseDistanceArr[i] << "\t\t\t\t\t" << maxWidthArr[i] << endl;
+    }
+    
+}
+
+void intializeBaseDistanceArr(){
+    baseDistanceArr[0] = 0;
+    //baseDistanceArr[1] = baseDistanceArr[0] + maxWidthArr[0];
+    for(int i=1; i<alphabetSize; i++){
+        baseDistanceArr[i] = baseDistanceArr[i-1] + maxWidthArr[i-1];
     }
 }
 
@@ -27,11 +54,92 @@ cv::Mat state(cv::Mat currentState, int action, int glyphID){
     return nextState;
 }
 
+cv::Mat gernerateCurrentState(){
+    cv::Mat currentState = glyphMatArr[0];
+    cv::Mat gapMat;
+    int gapSize;
+    
+    for(int i=1;i<alphabetSize;i++){
+        gapSize = (baseDistanceArr[i] -(baseDistanceArr[i-1] + maxWidthArr[i-1]));
+        gapMat = cv::Mat::zeros(currentState.rows, gapSize,CV_8U);
+        //printGlyph(gapMat);
+        //cout << "DDDD"<< currentState.type()<<endl;
+        cv::hconcat(currentState, gapMat, currentState);
+        cv::hconcat(currentState, glyphMatArr[i], currentState);
+    }
+    stateMat = currentState;
+    return currentState;
+}
+////
+cv::Mat gernerateState(){
+    cv::Mat currentState = glyphMatArr[0];
+    cv::Mat gapMat;
+    cv::Mat leftPart,rightPart,commanPart,prevState,temp;
+    kernValueArr[0] = -10;
+    //kernValueArr[1] = -10;
+    for(int i=1;i<alphabetSize;i++){
+        if(kernValueArr[i-1]==0){
+            //cout << glyphMatArr[i].type()<<endl;
+            cv::hconcat(currentState, glyphMatArr[i], currentState);
+        }
+        else if(kernValueArr[i-1] >0){
+            gapMat = cv::Mat::zeros(currentState.rows, kernValueArr[i-1],CV_8U);
+            cv::hconcat(currentState, gapMat, currentState);
+            cv::hconcat(currentState, glyphMatArr[i], currentState);
+            
+            
+        }
+        else if(kernValueArr[i-1] <0){
+            //cv::hconcat(currentState, glyphMatArr[i], currentState);
+            leftPart = currentState.colRange((currentState.cols-1)-(kernValueArr[i-1]*-1) , (currentState.cols-1));
+            rightPart = glyphMatArr[i].colRange(0, (kernValueArr[i-1]*-1));
+            addWeighted( leftPart, 1, rightPart, 1, 0.0, commanPart);
+            printGlyph(commanPart);
+            
+            leftPart = currentState.colRange(0, (currentState.cols-1)-(kernValueArr[i-1]*-1));
+            rightPart = glyphMatArr[i].colRange((kernValueArr[i-1]*-1),glyphMatArr[i].cols-1);
+            cv::hconcat(leftPart, commanPart, currentState);
+            cv::hconcat(currentState, rightPart, currentState);
+        }
+    }
+
+    stateMat = currentState;
+    return currentState;
+}
+
+void moveLeft(int glypId){
+    cv::Mat leftPart,rightPart,commanPart,prevState,temp;
+    
+    if(baseDistanceArr[0] + maxWidthArr[0] > baseDistanceArr[1] ){
+        
+    }
+    else if(baseDistanceArr[0] + maxWidthArr[0] < baseDistanceArr[1] ){
+        
+    }
+    else{
+        leftPart = glyphMatArr[0].colRange(glyphMatArr[0].cols-20-1, glyphMatArr[0].cols-1);
+        rightPart = glyphMatArr[1].colRange(0, 20);
+        addWeighted( leftPart, 1, rightPart, 1, 0.0, commanPart);
+        printGlyph(commanPart);
+        prevState = stateMat;
+        
+        leftPart = prevState.colRange(0, baseDistanceArr[0] + maxWidthArr[0] - 20-2);
+        rightPart = prevState.colRange(baseDistanceArr[1]+20-1 , prevState.cols-1);
+        //printGlyph(rightPart);
+        cv::hconcat(leftPart, commanPart, temp);
+        cv::hconcat(temp, rightPart, temp);
+        printGlyph(temp);
+    }
+
+    
+}
+
 
 class Glyph
 {
 private:
     static int glyphId;
+    int id;
     int maxWidth;
     cv::Mat glyph;
 
@@ -40,6 +148,7 @@ public:
     {
         
         cout << "glyphId is: " << glyphId << "\n";
+        id = glyphId;
         glyphId++;
     }
     
@@ -54,7 +163,7 @@ public:
                 count += (int)glyphInput.at<uchar>(j,i);
             }
             if (count != 0){
-                startcol = i - 1;
+                startcol = i ;
                 break;
             }
             
@@ -74,8 +183,11 @@ public:
         }
         
         maxWidth = endcol - startcol +1;
-        //cout << startcol << " " << endcol ;
+        maxWidthArr[id] = maxWidth;
+        
+        cout << startcol << " " << endcol << " " << maxWidth;
         glyph = glyphInput.colRange(startcol, endcol);
+        glyphMatArr[id] = glyph;
     }
     
     cv::Mat getGlyphMat(){
@@ -87,7 +199,6 @@ int Glyph::glyphId = 0;
 
 int main(int argc, char** argv)
 {
-    int alphabetSize = 3;
     std::string glyphPath;
     Glyph glyphObject[alphabetSize];
     cv::Mat glyph;
@@ -105,12 +216,13 @@ int main(int argc, char** argv)
         glyphObject[i].setGlyphMat(glyph);          // open image
         glyph = glyphObject[i].getGlyphMat();
 
-        cv::imshow("xx", glyph);
-        waitKey(5000);
+        //cv::imshow("xx", glyph);
+        //waitKey(5);
         
     }
-    
-    
-    
+    intializeBaseDistanceArr();
+    printStats();
+    printGlyph(gernerateState());
+    //moveLeft(1);
     return 0;
 }
